4c9149f78368329f1a6302f25e4b24ecf4f5b774
Krisztian Ivan <krisztian.ivan@gmail.com>
2022-03-17 13:21:32 +0000

    get a single employee name

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          List<string> employees = new List<string>();
+
+          while (true)
+          {
+          Console.WriteLine("Please enter a name: (leave empty to exit): ");
+          string input = Console.ReadLine();
+          if (input == "")
+          {
+              break;
+          }
+          employees.Add(input);
+          }
+
+          for (int i = 0; i < employees.Count; i++)
+          {
+              Console.WriteLine(employees[i]);
+          }
+        }
+    }
+}
