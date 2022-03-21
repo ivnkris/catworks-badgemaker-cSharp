fb672476301546f888079953ddc8d7442cadcf74
Krisztian Ivan <krisztian.ivan@gmail.com>
2022-03-21 12:20:08 +0000

    write employee details into csv file

--- Util.cs
+++ Util.cs
@@ -24,7 +24,12 @@ namespace CatWorx.BadgeMaker
 
             using (StreamWriter file = new StreamWriter("data/employees.csv"))
             {
-                
+                file.WriteLine("ID,Name,PhotoUrl");
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+                }
             }
         }
     }
