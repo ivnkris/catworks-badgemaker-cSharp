dfdb865786b00ea1ea4cd28a140a30eaff4b4749
Krisztian Ivan <krisztian.ivan@gmail.com>
2022-03-21 10:49:42 +0000

    collecting and printing all Employee property values

--- Program.cs
+++ Program.cs
@@ -16,14 +16,21 @@ namespace CatWorx.BadgeMaker
             List<Employee> employees = new List<Employee>();
             while (true)
             {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine();
-                if (input == "")
+                Console.WriteLine("Enter first name: (leave empty to exit): ");
+                string firstName = Console.ReadLine();
+                if (firstName == "")
                 {
                     break;
                 }
 
-                Employee currentEmployee = new Employee(input, "Smith");
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine();
+
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
                 employees.Add(currentEmployee);
           }
             return employees;
