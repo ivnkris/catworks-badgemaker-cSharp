2cb00152015f639d8fb91e222a7a9395ce18c401
Krisztian Ivan <krisztian.ivan@gmail.com>
2022-03-26 09:04:54 +0000

    call API

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker {
@@ -6,6 +7,13 @@ namespace CatWorx.BadgeMaker {
     {
         public static List<Employee> GetFromApi() {
             List<Employee> employees = new List<Employee>();
+
+            using (WebClient client = new WebClient())
+            {
+                string response = client.DownloadString("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+                Console.WriteLine(response);
+            }
+
             return employees;
         }
     }
