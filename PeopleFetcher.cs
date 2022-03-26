62079f04bcd00f113955705840cdcc67c27d932c
Krisztian Ivan <krisztian.ivan@gmail.com>
2022-03-26 09:19:46 +0000

    create employee badges using API

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 namespace CatWorx.BadgeMaker {
     class PeopleFetcher
@@ -11,7 +12,17 @@ namespace CatWorx.BadgeMaker {
             using (WebClient client = new WebClient())
             {
                 string response = client.DownloadString("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
-                Console.WriteLine(response);
+                JObject json = JObject.Parse(response);
+                foreach (JToken token in json.SelectToken("results")) {
+                    Employee emp = new Employee
+                    (
+                        token.SelectToken("name.first").ToString(),
+                        token.SelectToken("name.last").ToString(),
+                        Int32.Parse(token.SelectToken("id.value").ToString().Replace("-", "")),
+                        token.SelectToken("picture.large").ToString()
+                    );
+                    employees.Add(emp);
+                }
             }
 
             return employees;
